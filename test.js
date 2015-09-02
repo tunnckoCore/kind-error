0ddcf3d656bc0ea84b333b58dbd1601d881e5b6b
tunnckoCore <mameto_100@mail.bg>
2015-09-03 01:07:24 +0300

    add one more test for toString

--- test.js
+++ test.js
@@ -122,6 +122,23 @@ test('kind-error:', function () {
     test.equal(err instanceof Error, true)
     done()
   })
+  test('should have better .toString() when `actual` and `expected`', function (done) {
+    var err = new KindError({
+      name: 'BetterError',
+      actual: {foo: 'bar'},
+      expected: 'string'
+    })
+    var str = err.toString()
+
+    test.ok(str.indexOf('actual: object') !== -1)
+    test.ok(str.indexOf('expected: string') !== -1)
+    test.ok(str.indexOf('BetterError: expect `string`, but `object` given') !== -1)
+    test.equal(err.toString().length > 10, true)
+    test.equal(typeof err.toString, 'function')
+    test.equal(typeof err.toString(), 'string')
+    test.equal(err instanceof Error, true)
+    done()
+  })
   test('should create `message`, if only `actual` and `expected` given', function (done) {
     var err = new KindError({
       name: 'AssertError',
